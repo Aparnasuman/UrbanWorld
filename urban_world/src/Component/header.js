a7566e2016c5dfcab99865c6aec2286236c17d02
Aparna Suman <Aparnasuman2@gmail.com>
2020-10-14 08:13:16 +0530

    just changes

--- urban_world/src/Component/header.js
+++ urban_world/src/Component/header.js
@@ -2,47 +2,48 @@ import React from 'react'
 import '../App.css';
 const Header = (props) => {
     return (
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container-fluid  container">
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container-fluid  container">
             <div className="navbar navbar-expand-lg navbar-light bg-light">
-              <a className="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+              <a className="navbar-brand" href="./Home">Navbar</a>
+              <form className="form-inline my-2 my-lg-0">
+               <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+           </form>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false\" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-   </form>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+  
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="./Home">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+      <li className="nav-item">
+        <a className="nav-link" href="./Home">Link</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="./Home" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="./Home">Action</a>
+          <a className="dropdown-item" href="./Home">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="./Home">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="./Home">Disabled</a>
       </li>
     </ul>
    
     </div>
             </div>
-            <div class="row justify-content-end">
-           <div class="col-4 mt-8">
+            <div className="row justify-content-center">
+           <div className="col-4 mt-8">
               <h6>Urban</h6>
               <h1>UrbanWorld</h1>
-              <button className="btn btn-warning rounded-pill">CONTACT</button>
+              <button className="btn btn-warning rounded-pill btn-lg">CONTACT</button>
             </div>
     
   </div>
